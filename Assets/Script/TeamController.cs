99605caf4166a728e28f843fe0c977acebfffd07
ikke123123 <45997197+ikke123123@users.noreply.github.com>
2020-02-15 19:37:59 +0100

    My dude

---
+++ Assets/Script/TeamController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamController : MonoBehaviour
+{
+    [Header("Team Selector")]
+    [SerializeField] private Team team;
+
+    [Header("Settings")]
+    [SerializeField] private float bruh;
+
+    [Header("Don't Touch")]
+    [SerializeField] private GameObject assassin;
+    [SerializeField] private GameObject archer;
+    [SerializeField] private GameObject tank;
+
+
+    /*Required options:
+     *Public:
+     * - Activate abilities
+     * - Walk
+     * - Activate normal attack
+     * 
+     *Private:
+     * - Health:
+     *      - Maintain current health
+     *      - Healing when within certain region
+     * - Receive damage:
+     *      - Damage with damage number
+     *      - 
+     */
+}
